d2c676349a4ce6e15601b737958b9ef261182e68
sa553 <aggarwalsatyam1@gmail.com>
2023-12-31 20:05:42 +0530

    Updates

---
+++ precache-manifest.0ae51bc80350e1af38ca516e2b29196c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165a11076b7d2dce1803d43d3afe0f4a",
+    "url": "/myfirstwebapp/index.html"
+  },
+  {
+    "revision": "460c953c3405d5105f55",
+    "url": "/myfirstwebapp/static/css/main.977c4aae.chunk.css"
+  },
+  {
+    "revision": "b723b06a772ed3edf803",
+    "url": "/myfirstwebapp/static/js/2.50d62705.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/myfirstwebapp/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "460c953c3405d5105f55",
+    "url": "/myfirstwebapp/static/js/main.f3b211a4.chunk.js"
+  },
+  {
+    "revision": "66c50373f124cd31b47a",
+    "url": "/myfirstwebapp/static/js/runtime~main.ace3d4a4.js"
+  }
+]);
